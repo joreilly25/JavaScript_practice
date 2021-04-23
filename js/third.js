9f3196dc425ceeb1313611529d696c2c7aa6f075
Justin O'Reilly <joreilly25@gmail.com>
2021-04-23 16:34:24 -0400

    JS Functions!

---
+++ js/third.js
@@ -0,0 +1,38 @@
+
+
+function favoriteAnimal(animal) {
+    console.log(animal + " is my favorite animal!")
+  }
+  
+  favoriteAnimal('Cheetah');
+
+  function add7(number) {
+      let numberAdded7 = number + 7;
+      console.log(numberAdded7);
+  }
+
+  add7(7);
+
+  function multiply(num1, num2) {
+      let numProduct = num1 * num2;
+      console.log(numProduct);
+  }
+
+  multiply(5,5);
+
+  function capitalize(string) {
+      let capFirst = string.charAt(0).toUpperCase();
+      let stringCap = capFirst + string.substr(1).toLowerCase();
+      console.log(stringCap);
+}
+
+capitalize("HeLoLo");
+
+function lastLetter(findLastLetter) {
+    let stringLength = findLastLetter.length;
+    stringLength--
+    let foundLastLetter = findLastLetter.charAt(stringLength);
+    console.log(foundLastLetter);
+}
+
+lastLetter("HELLO BEAUTIFUL");
